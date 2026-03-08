42856fc29271fab3c9cb2bfe8642257ae96924b4
SRINESH G <srineshsrinesh80@gmail.com>
2026-03-08 20:23:32 +0530

    hide

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,4 +1,13 @@
 import streamlit as st
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 import pickle
 import pandas as pd
 import plotly.graph_objects as go
